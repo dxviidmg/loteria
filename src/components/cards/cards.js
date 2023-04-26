acd1162643e9af931be797baf57b92c117cc7a0d
David <davidmejia.05@hotmail.com>
2023-04-25 18:03:19 -0600

    api

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,4 +1,6 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from 'axios';
+
 
 export function Card({card, index, onClick, style}){
     return (
@@ -11,13 +13,10 @@ export function Card({card, index, onClick, style}){
 
 export function Cards(){
 
-  const cards_default = [
-    {image: 'https://www.todopapas.com/img/cuentos/9/8/1/981.jpg', name: 'El gallo', number: 1},
-    {image: 'https://m.media-amazon.com/images/I/512L8Zsm8+L._AC_SX425_.jpg', name: 'L', number: 2},
-    {image: 'https://www.todopapas.com/img/cuentos/9/8/1/981.jpg', name: 'El gallo', number: 3},
-    {image: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/599/697/products/loterriamexicana1-9bbc17150ca7af307816268844916702-640-0.jpg', name: 'Loteria', number: 4}]
-    
-  const [cards, setCards] = useState(cards_default)
+
+  
+//  console.log(cards_api)
+  const [cards, setCards] = useState([])
 
   const handleDelete = (index) => {
     const new_cards = [...cards];
@@ -25,6 +24,19 @@ export function Cards(){
     setCards(new_cards);
     console.log(new_cards, cards)
   };
+
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/cards/')
+      .then(response => {
+        let limit = Math.round(response.data.length*0.3)
+        setCards(response.data.slice(0, limit));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
 
   if (cards.length === 0){
     return <h1>Ganaste</h1>

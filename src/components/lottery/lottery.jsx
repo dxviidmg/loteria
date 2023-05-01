7d2e344780812336e36f7092705ed556191008bf
David <davidmejia.05@hotmail.com>
2023-05-01 14:27:13 -0600

    add api

--- src/components/lottery/lottery.jsx
+++ src/components/lottery/lottery.jsx
@@ -1,40 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const images = [
-    {image: 'https://www.todopapas.com/img/cuentos/9/8/1/981.jpg', name: 'El gallo', number: 1},
-    {image: 'https://m.media-amazon.com/images/I/512L8Zsm8+L._AC_SX425_.jpg', name: 'L', number: 2},
-    {image: 'https://www.todopapas.com/img/cuentos/9/8/1/981.jpg', name: 'El gallo', number: 3},
-    {image: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/599/697/products/loterriamexicana1-9bbc17150ca7af307816268844916702-640-0.jpg', name: 'Loteria', number: 4}
-];
+/*
+1 arreglar el renderizado de los axios
+2 Entender useeffect
+3 Entender logica de linea [isPlaying, cards.length]
+
+*/
 
 export function Lottery() {
   const [currentImage, setCurrentImage] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [cards, setCards] = useState([]);
+
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/cards/")
+      .then((response) => {
+        setCards(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
 
   useEffect(() => {
     let intervalId;
     if (isPlaying) {
       intervalId = setInterval(() => {
-        setCurrentImage(currentImage => (currentImage + 1) % images.length);
+        setCurrentImage((currentImage) => (currentImage + 1) % cards.length);
       }, 2000);
     }
+    return () => clearInterval(intervalId); // cleanup function to stop the interval when the component unmounts
+  }, [isPlaying, cards.length]);
 
-    return () => clearInterval(intervalId);
-  }, [isPlaying]);
+  useEffect(() => {
+    if (currentImage === cards.length - 1) {
+      setIsPlaying(false);
+    }
+  }, [currentImage, cards.length]);
 
-  function handlePlayButtonClick() {
-    setIsPlaying(true);
-  }
-
-  function handleStopButtonClick() {
-    setIsPlaying(false);
-  }
+  const togglePlaying = () => {
+    setIsPlaying((isPlaying) => !isPlaying);
+  };
 
   return (
     <div>
-      <button onClick={handlePlayButtonClick}>Iniciar presentación</button>
-      <button onClick={handleStopButtonClick}>Detener presentación</button>
-      <img src={images[currentImage]} alt={`Imagen ${currentImage + 1}`} />
+      <button onClick={togglePlaying}>Iniciar presentación</button>
+      <img
+        src={cards[currentImage].image}
+        alt={`Imagen ${currentImage + 1}`}
+      />
     </div>
   );
 }

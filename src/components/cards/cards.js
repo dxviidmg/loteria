0639718faf024acde7499b2272dac5a1e461da94
David <davidmejia.05@hotmail.com>
2023-05-01 13:46:46 -0600

    fix css 2

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,57 +1,84 @@
-import { useState, useEffect } from "react"
-import axios from 'axios';
-import '../../App.css'
-import './cards.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "../../App.css";
+import "./cards.css";
+import { Link } from "react-router-dom";
 
-export function Card({card, index, onClick, style}){
-    return (
-        <div className="card">
-        <img className="image-card" src={card.image} alt={card.alt} style={style} onClick={() => onClick(index)}/>
-        <p>{card.number} {card.name}</p>
+
+export function Card({ card, index, onClick }) {
+  return (
+    <div className="card">
+      <div className="card-div">
+        <img
+          className="image-card"
+          src={card.image}
+          alt={card.alt}
+          onClick={() => onClick(index)}
+        />
       </div>
-    )
+      <p>
+        {card.number} {card.name}
+      </p>
+    </div>
+  );
 }
 
-export function Cards(){
-
-
-  
-//  console.log(cards_api)
-  const [cards, setCards] = useState([])
+export function Cards() {
+  //  console.log(cards_api)
+  const [cards, setCards] = useState([]);
 
   const handleDelete = (index) => {
     const new_cards = [...cards];
     new_cards.splice(index, 1);
     setCards(new_cards);
-    console.log(new_cards, cards)
+    console.log(new_cards, cards);
   };
 
-
   useEffect(() => {
-    axios.get('http://localhost:8000/api/cards/')
-      .then(response => {
-        let limit = Math.round(response.data.length*0.3)
+    axios
+      .get("http://localhost:8000/api/cards/")
+      .then((response) => {
+        let limit = Math.round(response.data.length * 0.3);
         setCards(response.data.slice(0, limit));
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   }, []);
- 
-  if (cards.length === 0){
-    return <h1>Ganaste</h1>
+
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
+  /*  const render = cards.map((card, index) => (
+    <div className="card-container" key={index}>
+      <Card card={card} index={index} onClick={handleDelete} />
+    </div>
+  ))
+
+
+  if (cards.length === 0) {
+    return <h1>Ganaste</h1>;
   }
+*/
 
   return (
-        
-        <div className='container cards-container'>
-        {cards.map((card, index) =>
-          <div className='card-container' key={index}>
-            <Card card={card} index={index} onClick={handleDelete}/>
-          </div>
-          
-
+    <div className="container">
+      <h1>Tablero</h1>
+      {cards.length === 0 ? (
+        <div className="winner-container">
+          <h1>Ganaste</h1>
+          <button className="menu-button" onClick={handleRefresh}>Jugar otra vez</button>
+        </div>
+      ) : (
+        <div className="cards-container">
+          {cards.map((card, index) => (
+            <div className="card-container" key={index}>
+              <Card card={card} index={index} onClick={handleDelete} />
+            </div>
+          ))}
+        </div>
       )}
-
-</div>)
+    </div>
+  );
 }
